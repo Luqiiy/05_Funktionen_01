5af4f26c071cde7eea22eaa2d88dab099dea9136
Luqiiy <a.schlotte97@gmail.com>
2021-05-20 11:03:02 +0200

    ++Funktionen

---
+++ script_01.js
@@ -0,0 +1,79 @@
+
+/***** Funktionen 01 *****/
+
+// 1. Kapselung von Codeblöcken
+
+
+// Funktionsrumpf (body)
+function test()
+{
+    console.log("Moin Hans");
+}
+
+// Funktionsaufruf ( call )
+
+//test();
+
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung! + Datenübergabe von INNEN
+
+function ausgabeName() 
+{
+    let firstName = "Deniz";            //--> Variable --> lokal scope
+    console.log(" Hallo " + firstName + "!");
+}
+
+    //ausgabeName();
+    //console.log(firstName);     // Fehler -->Lokal!!!
+
+
+    /***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUßEN
+
+function ausgabeNameParam(firstName) //Parameter
+{
+    //wenn firstName leer , dann "nobody"
+    if (firstName == undefined || firstName == "") {
+        firstName = "nobody"
+
+    }
+
+       console.log(" Hallo " + firstName + "!");
+}
+
+ausgabeNameParam("Deniz");          // Call + Argument(e)
+ausgabeNameParam("Frank");
+ausgabeNameParam();
+ausgabeNameParam(prompt("gib hier deinen Namen ein!"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
